5f59f9fb7b573750409e84f0acb2a42d0720f850
Lim Yi Qin <LIMYIQIN.15@ichat.sp.edu.sg>
2017-11-29 22:07:55 +0800

    fix some issues with bot not sending message on schedule

--- advent.js
+++ advent.js
@@ -1,42 +1,45 @@
-// Import the discord.js module
-const Discord = require('discord.js');
+// Import the discord.js module 
+const Discord = require('discord.js');  
+ 
+//Import CRON 
+var CronJob = require('cron').CronJob; 
 
-//Import CRON
-var CronJob = require('cron').CronJob;
+// Create an instance of a Discord client 
+ const client = new Discord.Client(); 
+ 
 
+// The token of your bot - https://discordapp.com/developers/applications/me 
+const token = '<bot token>'; 
 
-// Create an instance of a Discord client
-const client = new Discord.Client();
+ 
+// The ready event is vital, it means that your bot will only start reacting to information 
+// from Discord _after_ ready is emitted 
+client.on('ready', () => { 
+  console.log('Advert Bot Running!'); 
+}); 
 
-// The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'your bot token here';
+ 
+// Create an event listener for messages 
+client.on('message', message => { 
+  // If the message is "ping" 
+  if (message.content === '?mhadv ping') { 
+    // Send "pong" to the same channel 
+    message.channel.send('Pong!'); 
+  } 
+}); 
 
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-// Create an event listener for messages
-client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'a?ping') {
-    // Send "pong" to the same channel
-    message.channel.send('Pong!');
-  }
-});
-
-//for reminding
-var job = new CronJob({
-  cronTime: '00 00 08 * * 1-7',
-  onTick: function() {
-  	var channel = client.servers.get("bot-test", "Chio Bu's Lair").defaultChannel;
-	client.sendMessage(channel, "Hello World");
-  },
-  start: false,
-  timeZone: 'America/Los_Angeles'
-});
-job.start();
-
-// Log our bot in
-client.login(token);
+ 
+//for reminding 
+var job = new CronJob({ 
+  cronTime: '00 00 08 * * 0-6', 
+  onTick: function() { 
+	client.channels.get('<channel id>').send("It's a new day in Gnawnian Advent Calendar! Time to claim your prize for today!");
+  }, 
+  start: false, 
+  timeZone: 'Asia/Singapore' 
+}); 
+job.start(); 
+ 
+ 
+// Log our bot in 
+client.login(token); 
